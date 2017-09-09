23977fdecabd7a10052b9030e2144dee51f477b0
Guy Arnan <guyarnan@gmail.com>
2017-09-09 14:38:23 +0300

    initial commit

---
+++ src/app/products/product.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+
+import { IProduct } from './product';
+
+@Injectable()
+export class ProductService {
+    private _productUrl = 'https://api.mlab.com/api/1/databases/mydb/collections/products?apiKey=z7YZSQIxkANQ78X0G0bVjCedIiKOYVrP';
+
+    constructor(private _http: HttpClient) { }
+
+    getProducts(): Observable<IProduct[]> {
+        return this._http.get<IProduct[]>(this._productUrl)
+            .do(data => console.log('All: ' + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+
+    getProduct(id: number): Observable<IProduct> {
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        var errorMessage = '';
+        if (err.error instanceof Error) {
+            // A client-side or network error occurred. Handle it accordingly.
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return Observable.throw(errorMessage);
+    }
+}
